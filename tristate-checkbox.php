32ab74a6fb321ef94d337034011ef2d4fed84a33
ravi <ravi23verma@gmail.com>
2013-10-05 17:49:33 +0530

    Create tristate-checkbox.php
    commit 1

---
+++ tristate-checkbox.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Tristate Checkboxes</title>
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+		<script>
+		$( document ).ready(function() {
+		
+			// initiating all checkboxs unchecked (it can be anything)
+			$("input:checkbox").prop("indeterminate", false);
+			$("input:checkbox").data("checked", 0);
+			$("input:checkbox").prop("checked", false);
+		
+			//handling click event on checkboxes 
+			//assigning states
+			$('input:checkbox[class=indeterminate]').live('click', function(){
+				switch($(this).data("checked")) {
+				// unchecked, going indeterminate
+					case 0:
+						$(this).data("checked",1);
+						$(this).prop("indeterminate",true);
+					break;
+
+					// indeterminate, going checked
+					case 1:
+						$(this).data("checked",2);
+						$(this).prop("indeterminate",false);
+						$(this).prop("checked",true);                
+					break;
+
+					// checked, going unchecked
+					default:  
+						$(this).data("checked",0);
+						$(this).prop("indeterminate",false);
+						$(this).prop("checked",false);
+				}
+			});
+		
+		});
+		 
+		//catching states on form submission
+		function submittristateform(){
+			var checked_opts = [];
+			var unchecked_opts = [];
+			var indeterminate_opts = [];
+
+			$("input[type=checkbox]").each(function() {
+			
+			alert($(this).data('checked')+ "---"+$(this).prop('checked')+"---"+$(this).prop('indeterminate'));
+			
+				if($(this).data('checked') == 2){
+					checked_opts.push($(this).val());
+				}else if($(this).data('checked') == 1){
+					indeterminate_opts.push($(this).val());
+				}
+				else if($(this).data('checked') == 0 || "undefined"==$(this).data('checked')){
+					unchecked_opts.push($(this).val());
+				}
+			});
+			
+			alert(checked_opts);
+			alert(unchecked_opts);
+			alert(indeterminate_opts);
+		}
+		</script>
+		
+	</head>
+	<body>
+		<form name="input" action="" method="POST">
+			<input class='indeterminate' type="checkbox" name="opt1" value="option 1" >option 1<br>
+			<input class='indeterminate' type="checkbox" name="opt2" value="option 2" >option 2<br>
+			<input class='indeterminate' type="checkbox" name="opt3" value="option 3" >option 3<br><br>
+			<input type="submit" value="Submit" onclick="submittristateform()">
+		</form> 
+	</body>
+</html>
+
+<?php 
+?>
